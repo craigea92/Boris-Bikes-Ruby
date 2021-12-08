84c6ad97c652b55f4e58e742f9f744890e1545dc
Craig Eaton <craigea92@gmail.com>
2021-12-08 13:41:04 +0000

    Added an Instance Variable & Attr Reader, 7 Passing Tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,25 @@
 require 'docking_station'
 
 describe DockingStation do
+    
+    it { is_expected.to respond_to(:bike) }
     it { is_expected.to respond_to :release_bike }
+
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'releases working bikes' do
         bike = subject.release_bike
         expect(bike).to be_working
+    end
+
+    it 'docks a bike' do
+        bike = Bike.new
+        expect(subject.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.bike).to eq bike
     end
 end
